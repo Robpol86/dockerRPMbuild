5baeb73b91726efc9054fc37253c7761524a56b5
Robpol86 <robpol86@gmail.com>
2016-12-29 19:06:07 -0800

    Debug output.

--- build.sh
+++ build.sh
@@ -5,7 +5,24 @@ set -u  # Treat unset variables as errors and exit immediately.
 set -x  # Print commands run by script to standard out.
 set -o pipefail  # Exit script if any piped program fails instead of just the last program.
 
+error () {
+    >&2 echo -e "\x1b[31m$1\x1b[0m"
+    exit 1
+}
+
 BUILD_USER="${BUILD_USER:-rpm}"
+
+# Print directory contents.
+ls -la /home/${BUILD_USER}/rpmbuild
+for d in /home/${BUILD_USER}/rpmbuild/*; do
+    ls -la "$d/"
+done
+
+# Check if we have at least one spec file.
+for f in /home/${BUILD_USER}/rpmbuild/SPECS/*.spec; do
+    [ -e "$f" ] || error "No spec files found in /home/$BUILD_USER/rpmbuild/SPECS"
+    break
+done
 
 # Install build dependencies and download source files for every spec file.
 for f in /home/${BUILD_USER}/rpmbuild/SPECS/*.spec; do
