d68054a083e52694fbb7e5b376107f9c69bb7f6c
Robpol86 <robpol86@gmail.com>
2016-12-29 17:51:49 -0800

    Moving symlinking into Dockerfile. Run as root.
    Need to run dnf command as root. Hence script will now run as the root
    user within the container. Script will call rpmbuild command via su to
    run as a non-root user.
    
    Moving symlinking into Dockerfile since it makes sense. That file
    prepares the container, the script focuses on just building.

--- build.sh
+++ build.sh
@@ -5,21 +5,17 @@ set -u  # Treat unset variables as errors and exit immediately.
 set -x  # Print commands run by script to standard out.
 set -o pipefail  # Exit script if any piped program fails instead of just the last program.
 
-# Prepare VOLUME symlinks.
-for name in RPMS SRPMS SOURCES SPECS; do
-    rmdir "$HOME/rpmbuild/$name"
-    ln -s "/$name" "$HOME/rpmbuild/$name"
-done
+BUILD_USER="${BUILD_USER:-rpm}"
 
 # Install build dependencies and download source files for every spec file.
-for f in $HOME/rpmbuild/SPECS/*.spec; do
+for f in /home/${BUILD_USER}/rpmbuild/SPECS/*.spec; do
     dnf builddep -y --spec "$f"
-    spectool -g "$f" -C $HOME/rpmbuild/SOURCES
+    su ${BUILD_USER} -lc "spectool -g '$f' -C rpmbuild/SOURCES"
 done
 
 # Build all RPMs.
-for f in $HOME/rpmbuild/SPECS/*.spec; do
-    rpmbuild -ba "$f"
+for f in /home/${BUILD_USER}/rpmbuild/SPECS/*.spec; do
+    su ${BUILD_USER} -lc "rpmbuild -ba '$f'"
 done
 
 echo "Success"
